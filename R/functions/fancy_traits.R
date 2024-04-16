7e054a34995cbf18d7a63b63da9b88cb9804262d
audhalbritter <me@audhalbritter.com>
2024-04-16 09:03:07 +0200

    add leaf to traits

--- R/functions/fancy_traits.R
+++ R/functions/fancy_traits.R
@@ -18,12 +18,14 @@ fancy_trait_name_dictionary <- function(dat){
                                 dC13_permil = "δC13 ‰",
                                 dN15_permil = "δN15 ‰")) |>
     mutate(figure_names = case_match(trait_trans,
-                                     "plant_height_cm_log" ~ "Height~(cm)",
-                                     "dry_mass_g_log" ~ "Dry~mass~(g)",
-                                     "leaf_area_cm2_log" ~ "Area~(cm^2)",
-                                     "leaf_thickness_mm_log" ~ "Thickness~(mm)",
+                                     "plant_height_cm_log" ~ "Plant~height~(cm)",
+                                     "dry_mass_g_log" ~ "Leaf~dry~mass~(g)",
+                                     "leaf_area_cm2_log" ~ "Leaf~area~(cm^2)",
+                                     "leaf_thickness_mm_log" ~ "Leaf~thickness~(mm)",
                                      "ldmc" ~ "LDMC",
-                                     "sla_cm2_g" ~ "SLA~(cm^2*g^{-1})"))
+                                     "sla_cm2_g" ~ "SLA~(cm^2*g^{-1})")) |>
+    mutate(figure_names = factor(figure_names,
+                                 levels = c("Plant~height~(cm)", "Leaf~dry~mass~(g)", "Leaf~area~(cm^2)", "Leaf~thickness~(mm)", "SLA~(cm^2*g^{-1})", "LDMC")))
                                      # "C_percent" ~ "C~('%')",
                                      # "N_percent" ~ "N~('%')",
                                      # "CN_ratio" ~ "CN",
