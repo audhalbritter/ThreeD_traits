cb13098303f7846f1697bfffd0273abab63808f7
audhalbritter <me@audhalbritter.com>
2025-01-20 17:44:19 +0100

    fix dimensions for output

--- make.R
+++ make.R
@@ -13,8 +13,8 @@ fs::file_show("manuscript/manuscript.pdf")#display pdf
 ggsave("output/Figure2_WN_traits.png", trait_WN_figure, dpi = 300, height = 3, width = 7)
 ggsave("output/Figure3_WG_traits.png", trait_WG_figure, dpi = 300, height = 6, width = 6)
 ggsave("output/Figure4_pca.png", trait_pca_figure, dpi = 300, height = 6, width = 6)
-ggsave("output/Figure5_itv.png", itv_figure, dpi = 300, height = 8, width = 6)
+ggsave("output/Figure5_itv.png", itv_figure, dpi = 300, height = 6, width = 6)
 
 
-ggsave("output/FigureSIX_pca.png", full_trait_pca_figure, dpi = 300, height = 6, width = 6)
+ggsave("output/FigureS2_pca.png", full_trait_pca_figure, dpi = 300, height = 6, width = 6)
 ggsave("output/FigureSI_Y_itv.png", itv_origin_output, dpi = 300, height = 6, width = 6)
